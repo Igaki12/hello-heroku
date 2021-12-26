5f1634f6b4f1ab7aa758767e9aa111bd12423731
unknown <igatatsu1997@gmail.com>
2021-12-26 11:38:34 +0900

    Fixed a little

--- src/main/java/hello/main/HelloWorld.java
+++ src/main/java/hello/main/HelloWorld.java
@@ -1,10 +1,13 @@
 package hello.main;
 
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RestController;
 
+@SpringBootApplication
+@RestController
 public class HelloWorld {
 	
-	@RequestMapping("/")
+	
 	public static void main(String[] args) {
 		System.out.println("Hello World!!");
 	}
